7d14ab9a9fafe38305e7cf1b542650cae884bbd0
akdombrowski <akdflyinvee73@gmail.com>
2021-08-08 05:18:03 -0500

    v1.0.2 for 1.6.0 game version

--- SubModule.cs
+++ SubModule.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 
 using TaleWorlds.Core;
+using TaleWorlds.Engine;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
@@ -125,12 +126,67 @@ namespace DontInterruptMe
       Harmony.DEBUG = false;
     }
 
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Mission), "DecideWeaponCollisionReaction")]
+    protected static void DecideWeaponCollisionReaction(
+  Blow registeredBlow,
+  in AttackCollisionData collisionData,
+  Agent attacker,
+  Agent defender,
+  in MissionWeapon attackerWeapon,
+  bool isFatalHit,
+  bool isShruggedOff,
+  out MeleeCollisionReaction colReaction, Mission __instance)
+    {
+      Harmony.DEBUG = debug;
+      Debug.Print("");
+      FileLog.Log("");
+
+      // original code
+      if (collisionData.IsColliderAgent && collisionData.StrikeType == 1 && collisionData.CollisionHitResultFlags.HasAnyFlag<CombatHitResultFlags>(CombatHitResultFlags.HitWithStartOfTheAnimation))
+        colReaction = MeleeCollisionReaction.Staggered;
+      else if (!collisionData.IsColliderAgent && collisionData.PhysicsMaterialIndex != -1 && PhysicsMaterial.GetFromIndex(collisionData.PhysicsMaterialIndex).GetFlags().HasAnyFlag<PhysicsMaterialFlags>(PhysicsMaterialFlags.AttacksCanPassThrough))
+        colReaction = MeleeCollisionReaction.SlicedThrough;
+      else if (!collisionData.IsColliderAgent || registeredBlow.InflictedDamage <= 0)
+        colReaction = MeleeCollisionReaction.Bounced;
+      else if (collisionData.StrikeType == 1 && attacker.IsDoingPassiveAttack)
+        colReaction = MissionGameModels.Current.AgentApplyDamageModel.DecidePassiveAttackCollisionReaction(attacker, defender, isFatalHit);
+      else
+      {
+        WeaponClass weaponClass = !attackerWeapon.IsEmpty ? attackerWeapon.CurrentUsageItem.WeaponClass : WeaponClass.Undefined;
+        int num1 = attackerWeapon.IsEmpty ? 0 : (!isFatalHit ? 1 : 0);
+        int num2 = isShruggedOff ? 1 : 0;
+        colReaction = (num1 & num2) != 0 || attackerWeapon.IsEmpty && defender != null && defender.IsHuman && !collisionData.IsAlternativeAttack && (collisionData.VictimHitBodyPart == BoneBodyPartType.Chest || collisionData.VictimHitBodyPart == BoneBodyPartType.ShoulderLeft || (collisionData.VictimHitBodyPart == BoneBodyPartType.ShoulderRight || collisionData.VictimHitBodyPart == BoneBodyPartType.Abdomen) || collisionData.VictimHitBodyPart == BoneBodyPartType.Legs) ? MeleeCollisionReaction.Bounced : ((weaponClass == WeaponClass.OneHandedAxe || weaponClass == WeaponClass.TwoHandedAxe) && (!isFatalHit && (double)collisionData.InflictedDamage < (double)defender.HealthLimit * 0.5) || attackerWeapon.IsEmpty && !collisionData.IsAlternativeAttack && collisionData.AttackDirection == Agent.UsageDirection.AttackUp || collisionData.ThrustTipHit && (sbyte)collisionData.DamageType == (sbyte)1 && (!attackerWeapon.IsEmpty && defender.CanThrustAttackStickToBone(collisionData.CollisionBoneIndex)) ? MeleeCollisionReaction.Stuck : MeleeCollisionReaction.SlicedThrough);
+        if (!collisionData.AttackBlockedWithShield && !collisionData.CollidedWithShieldOnBack || colReaction != MeleeCollisionReaction.SlicedThrough)
+          return;
+        colReaction = MeleeCollisionReaction.Bounced;
+      }
+      // original code
+
+      if (defender != null && defender.IsMine && colReaction == MeleeCollisionReaction.Staggered)
+      {
+        Debug.Print("defender: " + defender.Name);
+        FileLog.Log("defender: " + defender.Name);
+        Debug.Print("Collision Reaction: cancel stagger");
+        FileLog.Log("Collision Reaction: cancel stagger");
+        Debug.Print("Collision Reaction: Bounced");
+        FileLog.Log("Collision Reaction: Bounced");
+        colReaction = MeleeCollisionReaction.Bounced;
+      }
+
+      Debug.Print("");
+      FileLog.Log("");
+      Harmony.DEBUG = false;
+    }
+
     protected override void OnBeforeInitialModuleScreenSetAsRoot()
     {
       base.OnBeforeInitialModuleScreenSetAsRoot();
       var harmony = new Harmony("com.DontInterruptMe.akdombrowski");
 
       harmony.PatchAll();
+
+      FileLog.Reset();
 
       InformationManager.DisplayMessage(new InformationMessage("Loaded 'AttackInteruptModifier'.", Color.FromUint(0182599992U)));
     }
